64cfa96feede8c70a30b401faaf4f08b2a32dd5a
seongm1n <0411tjdals34@gmail.com>
2025-05-28 15:53:12 +0900

    Initial commit

---
+++ src/main/java/academy/cheerlot/CheerlotApplication.java
@@ -0,0 +1,13 @@
+package academy.cheerlot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheerlotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheerlotApplication.class, args);
+	}
+
+}

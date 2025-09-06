68d71c21d41445e6aba82e6a1c39be05b135ca32
seongm1n <0411tjdals34@gmail.com>
2025-09-06 15:28:35 +0900

    refactor: 사용되지 않는 클래스 및 메서드 제거
    - RosterVersionService 전체 클래스 제거 (어디서도 사용되지 않음)
    - PlayerRepository의 사용되지 않는 메서드 제거:
      - deleteByTeamCode()
      - findByNameAndTeamCode()
    - TeamRepository의 사용되지 않는 메서드 제거:
      - findByName()
    - CheerSongRepository의 사용되지 않는 메서드 제거:
      - findByAudioFileName()
    - 불필요한 import 정리 (Optional 제거)

--- src/main/java/academy/cheerlot/team/TeamRepository.java
+++ src/main/java/academy/cheerlot/team/TeamRepository.java
@@ -4,12 +4,9 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface TeamRepository extends CrudRepository<Team, String> {
-    
-    Optional<Team> findByName(String name);
     
     List<Team> findAll();
 }
